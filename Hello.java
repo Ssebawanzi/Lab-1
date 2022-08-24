6a31de6585ea3d126c6be7119d37ef9cfa35f8fd
Ssebawanzi <112001694+Ssebawanzi@users.noreply.github.com>
2022-08-24 11:50:11 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*****************************************************
+ * Hello.Java
+ * ***************************************************
+ * Bulonziyee Ssebawanzi
+ * 8/24/2022
+ * CMSC-255-004-FA2022
+ * ***************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+	
+		System.out.println("Bulonziyee Ssebawanzi");
+		System.out.println("Hello.java");
+		System.out.println("Print lines program");
+		System.out.println("Created: 8/24/2022");
+		System.out.println("CMSC-255-004-FA2022");
+		System.out.println();
+	}
+}
